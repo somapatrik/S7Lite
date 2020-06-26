06886bddb53e2db0a0765b274d4edfe09b0cd05e
Patrik Šoma <soma.patrik@gmail.com>
2020-06-26 11:03:28 +0200

    Initial commit

---
+++ S7Lite/MainWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Snap7;
+
+namespace S7Lite
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        S7Server server;
+        byte[] DB1 = new byte[256];
+        Thread tserver;
+
+        Boolean run;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetGui();
+        }
+
+        private void SetGui()
+        {
+            cmb_ip.Items.Clear();
+            GetIp();
+        }
+
+        private void GetIp()
+        {
+            IPAddress[] localIPs = Dns.GetHostAddresses(Dns.GetHostName());
+            foreach (IPAddress ip in localIPs)
+            {
+                if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                {
+                    cmb_ip.Items.Add(ip.ToString()) ;
+                }
+            }
+
+            if (cmb_ip.HasItems) cmb_ip.SelectedIndex = 0;
+
+        }
+
+        private Boolean StartServer()
+        {
+            server = new S7Server();
+            server.RegisterArea(S7Server.S7AreaDB, 1, ref DB1, DB1.Length);
+            return server.StartTo(cmb_ip.SelectedItem.ToString()) == 0 ? true : false;
+        }
+
+        private void btn_connect_Click(object sender, RoutedEventArgs e)
+        {
+            if (StartServer())
+            {
+                run = true;
+                tserver = new Thread(() => { ServerWork(); });
+                tserver.Name = "S7Server";
+            }
+            
+        }
+
+        private void ServerWork()
+        {
+            while (run)
+            {
+
+            }   
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (tserver != null)
+            {
+                run = false;
+
+                if (tserver.IsAlive)
+                {
+                    tserver.Join(1000);
+
+
+                }
+            }
+        }
+    }
+}
