9d2ace4eca48785f4724c91b44a1b94bd2097385
Patrik Šoma <soma.patrik@gmail.com>
2020-07-22 20:44:05 +0200

    ļogger

---
+++ S7Lite/Class/Logger.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S7Lite
+{
+    public static class Logger
+    {
+
+        public static readonly object LogLock = new object() ;
+
+        public static void Logi(string msg)
+        {
+
+           lock(LogLock) {
+                
+                    if (!File.Exists("log.txt"))
+                    {
+                        File.Create("log.txt");
+                    }
+
+                    File.AppendAllText("log.txt", msg + Environment.NewLine);
+
+                    
+            }
+            
+        } 
+    }
+}
